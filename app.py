f4f1b71f11cbfd624efefbbeb0d981069e29c0c1
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-15 08:38:38 -0300

    Composição para pegar o valor das notas e guardar na lista

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_praca.alternar_estado()
 restaurante_praca.receber_avaliacao('Arruda', 10)
 restaurante_praca.receber_avaliacao('Thais', 8)
-# restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
-# restaurante_sushi = Restaurante('Naruto Sushi', 'Japonesa')
-# restaurante_sushi.alternar_estado()
+
 
 def main():
     Restaurante.listar_restaurantes()
