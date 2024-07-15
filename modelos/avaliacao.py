62efa9436e0424a837cf5bcc8d34b5afe2381ea2
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-15 10:16:08 -0300

    Adicionado as docstrings

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,5 +1,17 @@
 class Avaliacao:
+
+    """Representa a avaliação de um restaurante e suas características."""
+
     def __init__(self, cliente, nota):
+
+        """
+        Inicializa uma instância da Avaliação.
+
+        Parâmetros:
+        - Cliente: O cliente do restaurante.
+        - Nota: A nota que o cliente dar para o restaurante.
+        """
+
         self._cliente = cliente
         self._nota = nota
 
