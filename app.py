4245e76ff5d2b4b1632087e82423878d67af10a3
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-15 08:20:57 -0300

    Criação da classe avaliação

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
-restaurante_sushi = Restaurante('Naruto Sushi', 'Japonesa')
-restaurante_sushi.alternar_estado()
+restaurante_praca.receber_avaliacao('Arruda', 10)
+restaurante_praca.receber_avaliacao('Thais', 8)
+# restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+# restaurante_sushi = Restaurante('Naruto Sushi', 'Japonesa')
+# restaurante_sushi.alternar_estado()
 
 def main():
     Restaurante.listar_restaurantes()
