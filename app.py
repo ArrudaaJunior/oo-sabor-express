ae859a415c50ce9ca0284a44c1af9d3399696b32
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-16 08:46:46 -0300

    Refatoração do metodo de adicionar item no cardapio

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 4.0, 'grande')
 prato_paozinho = Prato('Pao de coco', 0.80, 'Pão de coco, o melhor da cidade')
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
     print(bebida_suco)
