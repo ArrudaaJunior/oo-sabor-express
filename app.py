4a667f95fe5f09f54b729c7539df6409f3bf65aa
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-16 10:03:36 -0300

    Conseguindo fazer requisição

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import requests
+
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebiba import Bebida
 from modelos.cardapio.prato import Prato
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+reponse = requests.get(url)
+
+if reponse.status_code == 200:
+    dados_json = reponse.json()
+    print(dados_json)
+else:
+    print(f'O erro foi {reponse.status_code}')
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 4.0, 'grande')
@@ -11,7 +22,8 @@ restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    restaurante_praca.exibir_cardapio
+    # restaurante_praca.exibir_cardapio
+    print(reponse)
 
 if __name__ == '__main__':
     main()
