2aa5ca242f3c096301c6a0b0938bb79a7416d6ea
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-16 10:42:13 -0300

    Filtrando dados

--- app.py
+++ app.py
@@ -9,9 +9,21 @@ reponse = requests.get(url)
 
 if reponse.status_code == 200:
     dados_json = reponse.json()
-    print(dados_json)
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_restaurante = item['Company']
+        if nome_restaurante not in dados_restaurante:
+            dados_restaurante[nome_restaurante] = []
+        
+        dados_restaurante[nome_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
 else:
     print(f'O erro foi {reponse.status_code}')
+
+print(dados_restaurante['McDonald’s'])
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 4.0, 'grande')
