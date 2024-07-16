73499575da3f4d891bd01453c5c27ca79497ff7c
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-16 09:13:44 -0300

    Utilizando polimorfismo para aplicar desconto nos itens com valores diferentes

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 4.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Pao de coco', 0.80, 'Pão de coco, o melhor da cidade')
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
